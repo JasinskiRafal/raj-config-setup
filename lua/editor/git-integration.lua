a80aee9ba9553faf32eadf41f6d37e15353f76fc
Rafal Jasinski <1jasinskirafal@gmail.com>
2025-01-01 15:32:30 +0100

    Moved gitsigns.lua to git-integration.lua

---
+++ lua/editor/git-integration.lua
@@ -0,0 +1,27 @@
+-- Adds git related signs to the gutter, as well as utilities for managing changes
+-- NOTE: gitsigns is already included in init.lua but contains only the base
+-- config. This will add also the recommended keymaps.
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim',
+    'lewis6991/gitsigns.nvim',
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  },
+  config = function()
+    require('telescope').load_extension 'lazygit'
+  end,
+}
