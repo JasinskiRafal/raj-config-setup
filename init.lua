ed7e849cadbdea890d7cb59f36cd0103ae210471
Rafal Jasinski <1jasinskirafal@gmail.com>
2025-01-04 22:31:22 +0100

    Removed unnecessary fluf

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
 require 'options'
-require 'keymappings'
 require 'autocommands'
 require 'health'
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -15,31 +12,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To chec the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup {
-  {
-    import = 'debugging',
-  },
-  {
-    import = 'editor',
-  },
-  {
-    import = 'file-navigation',
-  },
-  {
-    import = 'development',
-  },
+  { import = 'plugins' },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require 'keymappings'
