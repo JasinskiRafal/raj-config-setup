7b9d492c1cf40e5f4c341794f57ec781cf00dd97
Rafal Jasinski <rajs@softwaremind.com>
2025-03-21 15:52:10 +0100

    automated tree to close with debug open

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -22,9 +22,20 @@ return {
         },
       }
 
-      dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-      dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-      dap.listeners.before.event_exited['dapui_config'] = dapui.close
+      dap.listeners.after.event_initialized['dapui_config'] = function()
+        require('nvim-tree.api').tree.close()
+        dapui.open()
+      end
+
+      dap.listeners.before.event_terminated['dapui_config'] = function()
+        dapui.close()
+        require('nvim-tree.api').tree.open()
+      end
+
+      dap.listeners.before.event_exited['dapui_config'] = function()
+        dapui.close()
+        require('nvim-tree.api').tree.open()
+      end
     end,
   },
 }
